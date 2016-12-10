8bafc1fcac93c561ca0c07f0f879f83d0bb56a38
khoironi7887 <khoironi7887@students.amikom.ac.id>
2016-12-10 14:06:32 +0700

    caesarchiper belum vigenere

---
+++ Kriptografi2/src/kriptografi2/Kriptografi2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kriptografi2;
+
+/**
+ *
+ * @author Bob Rony
+ */
+public class Kriptografi2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
